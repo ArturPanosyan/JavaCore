5e4a6c3a65d09f48926dd520c72141fd4431839d
artur <artur.information@gmail.com>
2023-08-26 03:45:31 +0400

    add homework

---
+++ src/homework/Array.java
@@ -0,0 +1,42 @@
+package homework;
+
+public class Array {
+    public static void main(String[] args) {
+        int august[];
+        august = new int[31];
+        august[0] = 1;
+        august[1] = 2;
+        august[2] = 3;
+        august[3] = 4;
+        august[4] = 5;
+        august[5] = 6;
+        august[6] = 7;
+        august[7] = 8;
+        august[8] = 9;
+        august[9] = 10;
+        august[10] = 11;
+        august[11] = 12;
+        august[12] = 13;
+        august[13] = 14;
+        august[14] = 15;
+        august[15] = 16;
+        august[16] = 17;
+        august[17] = 18;
+        august[18] = 19;
+        august[19] = 20;
+        august[20] = 21;
+        august[21] = 22;
+        august[22] = 23;
+        august[23] = 24;
+        august[24] = 25;
+        august[25] = 26;
+        august[26] = 27;
+        august[27] = 28;
+        august[28] = 29;
+        august[29] = 30;
+        august[30] = 31;
+        System.out.println("Happy birthday to me, ogostosi "  + august[10] +  "" );
+
+    }
+}
+
