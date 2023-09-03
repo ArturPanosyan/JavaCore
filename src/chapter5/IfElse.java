51d3cc40dadd4043ad7f9d3624c3de479ffd58cc
artur <artur.information@gmail.com>
2023-09-03 16:10:38 +0400

    add homework

---
+++ src/chapter5/IfElse.java
@@ -0,0 +1,22 @@
+package chapter5;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int month = 4;
+        String season;
+        if (month == 12 || month == 1 || month == 2)
+            season = "Зиме";
+
+        else if (month == 3 || month == 4 || month == 5)
+            season = "Весне";
+        else if (month == 6 || month == 7 || month == 8)
+            season = "Лету";
+        else if (month == 9 || month == 10 || month == 11)
+            season = "Осени";
+        else
+            season = "вымышленным месяцам";
+        System.out.println("Апрель относится к " + season + " . ");
+
+
+    }
+}
