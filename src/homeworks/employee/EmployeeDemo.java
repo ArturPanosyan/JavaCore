d28b839695a168a2662e32f29931b988384fa3ab
artur <artur.information@gmail.com>
2023-10-03 19:53:59 +0400

    add homework

--- src/homeworks/employee/EmployeeDemo.java
+++ src/homeworks/employee/EmployeeDemo.java
@@ -3,18 +3,18 @@ package homeworks.employee;
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
-public class EmployeeDemo {
+    public class EmployeeDemo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         EmployeeStorage employeeStorage = new EmployeeStorage();
         boolean isRun = true;
         while (isRun) {
-            System.err.println("PLEASE INPUT FOR 0 Exit");
-            System.err.println("PLEASE INPUT FOR 1 ADD EMPLOYEE");
-            System.err.println("PLEASE INPUT FOR 2 PRINT ALL EMPLOYEE");
-            System.err.println("PLEASE INPUT FOR 3 SEARCH EMPLOYEE BY EMPLOYEE ID");
-            System.err.println("PLEASE INPUT FOR 4 SEARCH EMPLOYEE BY COMPANY NAME");
-            System.err.println("PLEASE INPUT FOR 5 SEARCH EMPLOYEE PHONE");
+            System.out.println("PLEASE INPUT FOR 0 Exit");
+            System.out.println("PLEASE INPUT FOR 1 ADD EMPLOYEE");
+            System.out.println("PLEASE INPUT FOR 2 PRINT ALL EMPLOYEE");
+            System.out.println("PLEASE INPUT FOR 3 SEARCH EMPLOYEE BY EMPLOYEE ID");
+            System.out.println("PLEASE INPUT FOR 4 SEARCH BY COMPANY NAME");
+            System.out.println("PLEASE INPUT FOR 5 SEARCH EMPLOYEE PHONE");
             String command = scanner.nextLine();
             switch (command) {
                 case "0":
@@ -37,7 +37,6 @@ public class EmployeeDemo {
                     double salary = Double.parseDouble(scanner.nextLine());
                     Employee employee = new Employee(name, surname, employeeID, phoneNumber, salary, companyName, position);
                     employeeStorage.add(employee);
-
                     break;
                 case "2":
                     System.err.println("--------------------");
@@ -46,18 +45,17 @@ public class EmployeeDemo {
                     break;
                 case "3":
                     System.err.println("Please input EmployeeID");
-                    String keyword = scanner.nextLine();
-                    employeeStorage.search(keyword);
+                    String keywordID = scanner.nextLine();
+                    employeeStorage.searchByID(keywordID);
                     break;
                 case "4":
                     System.err.println("Please input Company Name");
-                    keyword = scanner.nextLine();
-                    employeeStorage.search(keyword);
+                    String keywordCompany = scanner.nextLine();
+                    employeeStorage.searchByCompany(keywordCompany);
                     break;
                 default:
                     System.err.println(" Invalid command: Try again! ");
-
-
+                    break;
             }
 
 
--- src/homeworks/employee/EmployeeStorage.java
+++ src/homeworks/employee/EmployeeStorage.java
@@ -16,26 +16,37 @@ public class EmployeeStorage {
     public void print() {
         for (int i = 0; i < size; i++) {
             System.out.println(employees[i].getName() + " " + employees[i].getSurname() + " "
-            + employees[i].getEmployeeID() + " " + employees[i].getPhone() + " " +
+                    + employees[i].getEmployeeID() + " " + employees[i].getPhone() + " " +
                     employees[i].getCompany() + " " + employees[i].getPosition());
 
 
         }
     }
-public void search(String keyword) {
-    for (int i = 0; i < size; i++) {
-        if(employees[i].getEmployeeID().contains(keyword) || employees[i].getCompany().contains(keyword));
-        System.out.println(employees[i].getName() + " " + employees[i].getSurname() + " "
-                + employees[i].getEmployeeID() + " " + employees[i].getPhone() + " " +
-                employees[i].getCompany() + " " + employees[i].getPosition());
+
+    public void searchByID(String keywordID) {
+        for (int i = 0; i < size; i++) {
+            if (employees[i].getEmployeeID().contains(keywordID)) ;
+            System.out.println(employees[i].getName() + " " + employees[i].getSurname() + " "
+                    + employees[i].getEmployeeID() + " " + employees[i].getPhone() + " " +
+                    employees[i].getCompany() + " " + employees[i].getPosition());
+        }
+
     }
 
-}
+    public void searchByCompany(String keywordByCompany) {
+        for (int i = 0; i < size; i++) {
+            if (employees[i].getCompany().contains(keywordByCompany)) ;
+            System.out.println(employees[i].getName() + " " + employees[i].getSurname() + " "
+                    + employees[i].getEmployeeID() + " " + employees[i].getPhone() + " " +
+                    employees[i].getCompany() + " " + employees[i].getPosition());
+        }
+
+    }
 
     private void extend() {
-        Employee[] TempArray =new Employee[employees.length + 10];
-                System.arraycopy(employees,0, TempArray, 0 , employees.length);
-                employees = TempArray;
-        }
+        Employee[] TempArray = new Employee[employees.length + 10];
+        System.arraycopy(employees, 0, TempArray, 0, employees.length);
+        employees = TempArray;
     }
+}
 
